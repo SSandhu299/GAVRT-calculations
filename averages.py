b80f021c9c57b59c58a9f341f792f78189505a9c
SSandhu299 <sukhmans026@deltalearns.ca>
2025-01-30 13:04:39 -0800

    Update averages.py

--- averages.py
+++ averages.py
@@ -1,19 +1,25 @@
+# Lists that will fit observations
 first_baseline = []
 second_baseline = []
 
+# loops five times, asking for a baseline input and putting it into the selected lists
 for x in range(5):
     first_baseline.append(float(input(" 1st Baseline? ")))
 
 for x in range(5):
     second_baseline.append(float(input("2nd baseline? ")))
 
+# takes the numbers within the list and finds their average (average =  sum / number of objects - 5 in this case)
 average_of_first = sum(first_baseline)/5
 average_of_second = sum(second_baseline)/5
 
+# finds the sum of both 
 sum_of_both = average_of_first + average_of_second
 
+# finds average of both
 average_of_both = sum_of_both/2
 
+#returns average baseline values 
 print( "Your first baseline is:" , average_of_first)
 
 print( "Your second baseline is:", average_of_second)
